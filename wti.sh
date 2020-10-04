053fd09423267eecf35365eab904b637b12c5154
Marcin Borysiewicz <frugoxmaniak@gmail.com>
2020-10-04 19:47:22 +0200

    Added python package into apt-get command

--- wti.sh
+++ wti.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt install python-pip --yes
+apt install python python-pip --yes
 snap install pycharm-community --classic
 clear
 echo "Laboratorium testowe - zakończono proces instalacji"
