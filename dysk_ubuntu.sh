6bab2803c337b2000ee84aacc4641a67d07fac73
ninjaxfreezu <frugoxmaniak@gmail.com>
2020-10-07 00:47:28 +0200

    ~ Changed to $HOME

--- dysk_ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir ~/domowy
-sudo apt update --yes && sudo apt upgrade --yes
-sudo apt install sshfs
-sshfs -o password_stdin $1@pcss-home.cloud:/ ~/domowy/ <<< $2
-
-echo "Zamontowano dysk"
