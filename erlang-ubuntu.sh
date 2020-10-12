c42fe05ee09e717972dcd53000073729274d8f0a
Patryk <patryk.baryla@spoko.pl>
2020-10-12 15:22:07 +0200

    Erlang

---
+++ erlang-ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo dpkg -i erlang-solutions_1.0_all.deb
+sudo apt-get update
+sudo apt-get install esl-erlang
+
+echo "Programowanie wieloparadygmatowe - środowisko zostało skonfigurowane!"
