b9f513d71d5f915ccc32e159c72f5255b743c9b6
ninjaxfreezu <frugoxmaniak@gmail.com>
2020-11-18 15:17:54 +0100

    Added echoing error messages to stderr and exiting with code 1

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+# Functions declaraction
+echoerr() { echo "$@" 1>&2; }
+
 # THIS SCRIPT HAS TO BE EXECUTED BY ROOT.
 # Script installs all of virt-lab modules
 
 if [ $(whoami) != root ]
 then
-    echo "This script has to be executed by root. Use su or sudo to run that script."
+    echoerr "This script has to be executed by root. Use su or sudo to run that script."
     exit 1
 fi
 
-# TODO disable Ubuntu autoupdates!!!
+# Ubuntu auto-updates has to be disabled in order to guarantee a successfull installation
+# Make sure to not run any updates in the background as this script uses apt package manager
 
 # Installing curl
-apt install curl
+apt install curl || echoerr "Error while trying to install a package - apt package manager is currently being used by another process."; exit 1
 
 # Creating directory for storing modules data
 mkdir /etc/virt-lab
@@ -22,3 +26,6 @@ curl -LO https://raw.githubusercontent.com/NiNJAxFREEZu/inz-scripts/main/virt-la
 
 # Installing modules
 ./vm-communicator
+
+# Cleaning up, removing dowloaded scripts
+rm vm-communicator
