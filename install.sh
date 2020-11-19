03df7eeac9ee616d3cf4936454d2062e5c7c5d62
ninjaxfreezu <frugoxmaniak@gmail.com>
2020-11-19 18:43:43 +0100

    JK lol

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Functions declaraction
+echoerr() { echo "$@" 1>&2; }
+
+# Script installs all of VirtuaLab modules and launches them.
+
+# THIS SCRIPT HAS TO BE EXECUTED BY ROOT.
+# Ubuntu auto-updates has to be disabled in order to guarantee a successfull installation
+# Make sure to not run any updates in the background as this script uses apt package manager
+
+if [ $(whoami) != root ]; then
+    echoerr "This script has to be executed by root. Use su or sudo to run that script."
+    exit 1
+fi
+
+clear
+echo -e "██╗░░░██╗██╗██████╗░████████╗██╗░░░██╗░█████╗░██╗░░░░░░█████╗░██████╗░
+██║░░░██║██║██╔══██╗╚══██╔══╝██║░░░██║██╔══██╗██║░░░░░██╔══██╗██╔══██╗
+╚██╗░██╔╝██║██████╔╝░░░██║░░░██║░░░██║███████║██║░░░░░███████║██████╦╝
+░╚████╔╝░██║██╔══██╗░░░██║░░░██║░░░██║██╔══██║██║░░░░░██╔══██║██╔══██╗
+░░╚██╔╝░░██║██║░░██║░░░██║░░░╚██████╔╝██║░░██║███████╗██║░░██║██████╦╝
+░░░╚═╝░░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═════╝░\n\n***Installing VirtuaLab***\n"
+
+# Installing curl and git - needed to pull the rest of the dependencies and modules
+echo -ne "\tInstalling curl and git..."
+apt-get install curl git --yes > /dev/null || echoerr "Error while trying to install a package - apt package manager is currently being used by another process."
+echo " DONE"
+
+# Creating directory for storing modules data
+mkdir /etc/virtualab
+
+# Downloading install scripts
+echo -ne "\tPulling modules installation scripts..."
+curl -LOs https://raw.githubusercontent.com/NiNJAxFREEZu/inz-scripts/main/virt-lab-modules/vm-communicator.sh || echoerr "Error while trying to pull vm-communicator installation script."
+echo " DONE"
+
+### Installing modules
+echo -e "\n***Installing modules***\n"
+
+# VM-communicator
+echo -ne "\tInstalling vm-communicator..."
+chmod +x vm-communicator.sh
+./vm-communicator.sh
+echo " DONE"
+
+### Cleaning up, removing dowloaded scripts
+echo -ne "\tCleaning up..."
+rm vm-communicator.sh
+echo " DONE"
+
+echo -e "\nVirtuaLab has been installed succesfully!"
+exit 0
---
+++ virt-lab-modules/vm-communicator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Functions declaration
+echoerr() { echo "$@" 1>&2; }
+
+# Script installs communication module used to send text messages across VM's, 
+# checking attendance and collecting information about students activities on VM's.
+
+# installing dependencies 
+apt-get install python python3-pip git libnotify-bin --yes > /dev/null || echoerr "Error while trying to install a package - apt package manager is currently being used by another process."
+
+# cloning module's git repository
+git clone https://github.com/wranidlo/broadcast_sender_receiver /etc/virtualab/vm-communicator --quiet || echoerr "Error while trying to pull git repository for vm-communicator."
+chmod -R 777 /etc/virtualab/vm-communicator/
+
+# installing python dependencies
+pip3 install --no-input -r requirements.txt --quiet 
+
+# launching text-chat receiver
+cd /etc/virtualab/vm-communicator
+nohup python3 student_main.py &
