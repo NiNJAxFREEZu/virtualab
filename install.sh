c2f66e317b5b56d9235c1d27f3928d68f8d42b1c
ninjaxfreezu <frugoxmaniak@gmail.com>
2020-11-19 18:59:05 +0100

    Added some EEEE in error messages for better readabillity in the furute.

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ echo -e "██╗░░░██╗██╗██████╗░███�
 
 # Installing curl and git - needed to pull the rest of the dependencies and modules
 echo -ne "\tInstalling curl and git..."
-apt-get install curl git --yes > /dev/null || echoerr "Error while trying to install a package - apt package manager is currently being used by another process."
+apt-get install curl git --yes > /dev/null || echoerr "E: Error while trying to install a package - apt package manager is currently being used by another process."
 echo " DONE"
 
 # Creating directory for storing modules data
@@ -32,26 +32,29 @@ mkdir /etc/virtualab
 
 # Downloading install scripts
 echo -ne "\tPulling modules installation scripts..."
-curl -LOs https://raw.githubusercontent.com/NiNJAxFREEZu/inz-scripts/main/virt-lab-modules/vm-communicator.sh || echoerr "Error while trying to pull vm-communicator installation script."
-curl -LOs https://raw.githubusercontent.com/NiNJAxFREEZu/inz-scripts/main/virt-lab-modules/openmpi.sh || echoerr "Error while trying to pull OpenMPI installation script."
+curl -LOs https://raw.githubusercontent.com/NiNJAxFREEZu/inz-scripts/main/virt-lab-modules/vm-communicator.sh || echoerr "E: Error while trying to pull vm-communicator installation script."
+curl -LOs https://raw.githubusercontent.com/NiNJAxFREEZu/inz-scripts/main/virt-lab-modules/openmpi.sh || echoerr "E: Error while trying to pull OpenMPI installation script."
 echo " DONE"
 
 ### Installing modules
 echo -e "\n***Installing modules***\n"
 
 # VM-communicator
-echo -e "\tInstalling VM communicator..."
+echo -ne "\tInstalling VM communicator..."
 chmod +x vm-communicator.sh
 ./vm-communicator.sh
+echo " DONE"
 
 # OpenMPI module
-echo -e "\tInstalling OpenMPI..."
+echo -ne "\tInstalling OpenMPI..."
 chmod +x openmpi.sh
 # ./openmpi.sh TODO Piter
+echo " DONE"
 
 ### Cleaning up, removing dowloaded scripts
 echo -ne "\tCleaning up..."
 rm vm-communicator.sh
+rm openmpi.sh
 echo " DONE"
 
 echo -e "\nVirtuaLab has been installed succesfully!"
--- virt-lab-modules/vm-communicator.sh
+++ virt-lab-modules/vm-communicator.sh
@@ -7,10 +7,10 @@ echoerr() { echo "$@" 1>&2; }
 # checking attendance and collecting information about students activities on VM's.
 
 # installing dependencies 
-apt-get install python python3-pip git libnotify-bin --yes > /dev/null || echoerr "Error while trying to install a package - apt package manager is currently being used by another process."
+apt-get install python python3-pip git libnotify-bin --yes > /dev/null || echoerr "E: Error while trying to install a package - apt package manager is currently being used by another process."
 
 # cloning module's git repository
-git clone https://github.com/wranidlo/broadcast_sender_receiver /etc/virtualab/vm-communicator --quiet || echoerr "Error while trying to pull git repository for vm-communicator."
+git clone https://github.com/wranidlo/broadcast_sender_receiver /etc/virtualab/vm-communicator --quiet || echoerr "E: Error while trying to pull git repository for vm-communicator."
 chmod -R 777 /etc/virtualab/vm-communicator/
 
 # installing python dependencies
