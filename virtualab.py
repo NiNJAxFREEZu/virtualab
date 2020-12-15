418eeec48c6a4fe224be81f6a298b95b0c33015e
ninjaxfreezu <frugoxmaniak@gmail.com>
2020-12-15 21:31:11 +0100

    virtualab basic interpreter

--- virtualab.py
+++ virtualab.py
@@ -1,7 +1,39 @@
 import sys
 import json
 
-class_config_json_raw = open(sys.argv[1])
-class_config_json= json.loads(class_config_json_raw)
+def print_manpage():
+    with open("manpage", 'r') as manpage_file:
+        manpage = manpage_file.read()
 
-print(class_config_json)
+    print(manpage)
+
+def create_class_config():
+    print("TODO")
+
+def start_class(config_json):
+    print(config_json)
+
+if __name__ == "__main__":
+    # Interpreting command line arguments
+
+    if sys.argv[1] == "help":
+        print_manpage()
+
+    elif sys.argv[1] == "create":
+        create_class_config()
+
+    elif sys.argv[1] == "start":
+        with open(sys.argv[2], 'r') as file:
+            class_config_json_raw = file.read()
+
+        try:
+            class_config_json = json.loads(class_config_json_raw)
+        except ValueError as e:
+            sys.stderr.write(sys.argv[2] + " does not contain a valid JSON file.\n")
+            exit(1)
+
+        start_class(class_config_json)
+
+    else:
+        sys.stderr.write("\'" + sys.argv[1] + "\' is not a valid virtualab command. See \'virtualab help.\'\n")
+        exit(1)
