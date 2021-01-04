ee444876599f7b16cfccb54fd96af9e2a5912806
kingstiger <44239621+kingstiger@users.noreply.github.com>
2021-01-04 17:08:02 +0100

    Update openmpi_student.sh
    Just for WiP stuff

--- data/openmpi_student.sh
+++ data/openmpi_student.sh
@@ -6,13 +6,9 @@ sudo apt install openssh-server --yes
 sudo apt install ssh_askpass --yes
 sudo apt install hydra --yes
 
-#ip interfejsow i /etc/hosts do usuniecia przy przejsciu na vagranta
-# ip interfejsów
-sudo ifconfig $1 $2 netmask $3 up
+#ip interfejsow do wpisania /etc/hosts do wpisania przy przejsciu na vagranta
 
-echo "*** IP SET TO $2 AND NETMASK TO $3 ***\t"
-
-sudo echo "192.168.1.5	master" >> /etc/hosts
+sudo echo "192.168.101.2	master" >> /etc/hosts
 
 #w ~home robię /mirror, folder share'owany przez nfs
 cd ~
@@ -23,5 +19,3 @@ sudo mount master:/mirror /mirror
 #stwarzam usera z homedir w /mirror, żeby nie musieć klucza ssh przerzucać z clientów na mastera
 sudo useradd --home /mirror mpiuser
 echo "mpiuser:a" | sudo chpasswd
-
-sudo chown mpiuser /mirror
