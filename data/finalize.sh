ce4c8234d4ac930bcdfe1a22313b8689d1b8ae42
ninjaxfreezu <frugoxmaniak@gmail.com>
2021-01-15 23:08:51 +0100

    Added finalize.sh wrapper script for professor

---
+++ data/finalize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script completes the professor's .virtualabinfo and launches the second part of MPI configuration
+
+echo -ne "\tFinalizing virtualab cluster configuration..."
+cd /home/vagrant/data
+
+# TODO .virtualabinfo
+
+# MPI
+./mpi_prof_part_2.sh || exit 1
+
+echo " DONE"
