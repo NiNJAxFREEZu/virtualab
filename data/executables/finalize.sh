f1e74718a6eac49b9bc313c047b20ebca7945a5a
kingstiger <44239621+kingstiger@users.noreply.github.com>
2021-01-17 19:14:42 +0100

    Slight adjustment to beautify logs

--- data/executables/finalize.sh
+++ data/executables/finalize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script completes the professor's .virtualabinfo and launches the second part of MPI configuration
 
-echo -ne "\tFinalizing virtualab cluster configuration..."
+echo -ne "\tFinalizing virtualab cluster configuration...\t"
 cd /home/vagrant/data/executables
 
 # Gathering student's ip addresses for .virtualabinfo
